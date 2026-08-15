40e0d8e3fca813ff6a6c05c6665336008f90d676
Yoann Moreau <yoann.moreau@tutanota.com>
2026-08-15 19:58:58 +0200

    Prevent entity kills to drop items or XP while in Admin Mode

--- src/main/java/fr/ethilvan/adminMode/listeners/GameListeners.java
+++ src/main/java/fr/ethilvan/adminMode/listeners/GameListeners.java
@@ -1,9 +1,11 @@
 package fr.ethilvan.adminMode.listeners;
 
 import fr.ethilvan.adminMode.AdminMode;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import java.util.UUID;
@@ -29,5 +31,20 @@ public class GameListeners implements Listener {
 		}
 
 		adminMode.getStateManager().deactivateAdminMode(player);
+	}
+
+
+	@EventHandler
+	public void onEntityDeath(EntityDeathEvent event) {
+		Entity causingEntity = event.getDamageSource().getCausingEntity();
+		Entity deadEntity = event.getEntity();
+		if (!(causingEntity instanceof Player player) || deadEntity instanceof Player) return;
+		if (adminMode.getPlayerStatuses().get(player.getUniqueId()) == null ||
+				adminMode.getPlayerStatuses().get(player.getUniqueId()) != true) {
+			return;
+		}
+
+		event.getDrops().clear();
+		event.setDroppedExp(0);
 	}
 }
